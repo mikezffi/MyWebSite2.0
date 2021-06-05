5fff81bc8c635f39221a24170a8f620db9cf634c
mikezffi <michael.zuffi@gmail.com>
2021-06-05 13:26:34 -0300

    google store link

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "05a849aa64898c544111"; }
+/******/ 		__webpack_require__.h = function() { return "82691c1cf165389e5a2f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/Portfolio/index.tsx
+++ src/components/Portfolio/index.tsx
@@ -14,11 +14,11 @@ export function Portfolio() {
             <p>
               MyTimerPal is a mobile app that lets people organize their workout and time each exercise.
             </p>
-            <span>soon on:</span>
-            <img src="/appstores.png" alt="app store" style={{width: '100px', marginTop: '5px', marginBottom: '20px'}} />
+            <span>Get it on:</span>
+            <a href="https://play.google.com/store/apps/details?id=com.mikezffi.mytimerpal"><img src="/appstore.png" alt="app store" style={{width: '100px', marginTop: '5px', marginBottom: '20px'}} /></a>
 
             <p>I made this app using React Native with Expo CLI. It is multiplatform, has animated Lists and Lottie animations.</p>
-            <p>You can check out the code and a free demo on my <a href="https://github.com/mikezffi/MyTimerPal/releases/tag/v1.0">Github</a>.</p>
+            <p>You can check out the code on my <a href="https://github.com/mikezffi/MyTimerPal/releases/tag/v1.0">Github</a>.</p>
 
           </div>
         </div>
